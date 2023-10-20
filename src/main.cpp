aeaba7f8dc80ba15158bd39c1f83e4a370489dd2
Michael Haberler <git@mah.priv.at>
2023-10-20 12:51:47 +0200

    works with spdlog v1.12.0 and fmtlib mah-10.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 #undef F
 #include <fmt/core.h>
 
+#include <array>
+#include <numeric>
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/fmt/bin_to_hex.h"
+
+auto main_log = spdlog::stdout_color_mt("main", spdlog::color_mode::always);
+
 M5GFX display;
 
 static constexpr size_t BAR_COUNT = 64;
@@ -27,6 +36,21 @@ void setup(void)
   M5.begin(cfg);
 
   std::string s = fmt::format("The answer is {}.", 42);
+
+  spdlog::set_level(spdlog::level::trace); // Set global log level to trace
+  spdlog::set_pattern("[%t] [%C-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
+
+  main_log->trace("spdlog on ESP{}", 32);
+
+  std::string str{"ESP32"};
+  main_log->debug("  |{:20}|", str);   // |ESP32               |
+  main_log->info("   |{:<20}|", str);  // |ESP32               |
+  main_log->warn("|{:_>20}|", str);    // |_______________ESP32|
+  main_log->error("  |{:_^20}|", str); // |_______ESP32________|
+
+  std::array<char, 64> a;
+  std::iota(a.begin(), a.end(), 1);
+  main_log->critical("{:s:X}", spdlog::to_hex(a));
 
   display.init();
   display.startWrite();
