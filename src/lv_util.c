1594d164cf20478666565172bfba754988e53e41
Michael Haberler <git@mah.priv.at>
2023-11-29 19:57:02 +0100

    add lv_obj_bind_text_style_color widget binding

--- src/lv_util.c
+++ src/lv_util.c
@@ -20,6 +20,40 @@ void lv_subject_init_none(lv_subject_t *subject)
     _lv_ll_init(&(subject->subs_ll), sizeof(lv_observer_t));
 }
 
+#if LV_USE_USER_DATA
+static void text_style_color_observer_cb(lv_subject_t *subject, lv_observer_t *observer)
+{
+    lv_style_selector_t selector = (lv_style_selector_t)subject->user_data;
+    lv_color_t c = lv_subject_get_color(subject);
+    // LV_LOG_USER("--->  color=%ux\n", c);
+    lv_obj_t *target = (lv_obj_t *)lv_observer_get_target(observer);
+    lv_obj_set_style_text_color(target, c, selector);
+}
+
+static void text_style_color_changed_cb(lv_event_t *e)
+{
+    lv_obj_t *obj = lv_event_get_current_target(e);
+    lv_subject_t *subject = lv_event_get_user_data(e);
+    lv_style_selector_t selector = (lv_style_selector_t)subject->user_data;
+    lv_color_t c = lv_obj_get_style_text_color(obj, selector);
+    // LV_LOG_USER("--->  color=%ux\n", c);
+    lv_subject_set_color(subject, c);
+}
+
+lv_observer_t *lv_obj_bind_text_style_color(lv_obj_t *obj, lv_subject_t *subject,
+                                            lv_style_selector_t selector)
+{
+    if (subject->type != LV_SUBJECT_TYPE_COLOR)
+    {
+        LV_LOG_WARN("Incompatible subject type: %d", subject->type);
+        return NULL;
+    }
+    lv_obj_add_event_cb(obj, text_style_color_changed_cb, LV_EVENT_VALUE_CHANGED, subject);
+    lv_observer_t *observer = lv_subject_add_observer_obj(subject, text_style_color_observer_cb, obj, NULL);
+    return observer;
+}
+#endif
+
 void lvgl_assert_fail(void)
 {
     LV_LOG_USER("-------> ASSERT FAIL");
--- src/lv_util.h
+++ src/lv_util.h
@@ -22,7 +22,7 @@ extern "C"
     {
         subject->user_data = user_data;
     }
-    
+
     static inline void *lv_subject_get_user_data(lv_subject_t *subject)
     {
         return subject->user_data;
@@ -32,6 +32,17 @@ extern "C"
     lv_subject_type_t lv_subject_get_type(lv_subject_t *subject);
     void lv_subject_init_none(lv_subject_t *subject);
 
+#if LV_USE_USER_DATA
+    /**
+     * Bind an color subject to an object's text style color
+     * @param obj       pointer to an object
+     * @param subject   pointer to a subject
+     * @param selector  style selector to be used when applying the color
+     * @return          pointer to the created observer
+     */
+    lv_observer_t *lv_obj_bind_text_style_color(lv_obj_t *obj, lv_subject_t *subject,
+                                          lv_style_selector_t selector);
+#endif
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
