03dcfdb35dea09e0efa9541b2ef5449b80a1cdd8
Michael Haberler <git@mah.priv.at>
2024-01-26 18:22:18 +0100

    all.json: 2 tanks, env, oat

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "f6918cf-dirty";
+const char *GIT_REV = "989750d-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"switch to 'list of units' json format";
+const char *GIT_SUBJECT =	"all.json: 2 tanks, env, oat";
 const char *GIT_BRANCH =	"sensordesign";
-const char *GIT_COMMIT_DATE =	"Fri, 26 Jan 2024 13:32:39 +0100";
+const char *GIT_COMMIT_DATE =	"Fri, 26 Jan 2024 18:22:18 +0100";
 
 #endif
