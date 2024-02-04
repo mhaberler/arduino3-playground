26f691bf976410037ffff7796925959f80b9271e
Michael Haberler <git@mah.priv.at>
2024-02-04 21:33:19 +0100

    github problems today

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "3009dc3-dirty";
+const char *GIT_REV = "c08f931-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"fix sun7in nfc build, correct i2c - nfc works";
+const char *GIT_SUBJECT =	"fix spurious i2c error";
 const char *GIT_BRANCH =	"redo-tank-age";
-const char *GIT_COMMIT_DATE =	"Sun, 4 Feb 2024 17:40:17 +0100";
+const char *GIT_COMMIT_DATE =	"Sun, 4 Feb 2024 21:05:04 +0100";
 
 #endif
