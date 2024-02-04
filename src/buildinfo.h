3009dc35b157622f4f0abd8807d1651f8e55dca2
Michael Haberler <git@mah.priv.at>
2024-02-04 16:54:22 +0100

    fix sun7in nfc build, correct i2c - nfc works

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "391d7d5-dirty";
+const char *GIT_REV = "f2d1d78-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"seems to work";
+const char *GIT_SUBJECT =	"fix sun7in nfc build, correct i2c - nfc works";
 const char *GIT_BRANCH =	"redo-tank-age";
-const char *GIT_COMMIT_DATE =	"Sun, 4 Feb 2024 15:53:15 +0100";
+const char *GIT_COMMIT_DATE =	"Sun, 4 Feb 2024 16:54:22 +0100";
 
 #endif
