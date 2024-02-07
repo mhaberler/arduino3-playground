1c5e53748c569a6a559b865ea4b1a7a732f6d940
Michael Haberler <git@mah.priv.at>
2024-02-08 00:13:02 +0100

    lv_chart_set_point_count( <lv_obj_t*>, 0) crashes
    for now, set mintanks = 2
    
    todo: file bug with LVGL

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "355af5d-dirty";
+const char *GIT_REV = "c2d8b07-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"add live adding additional tanks";
+const char *GIT_SUBJECT =	"remove dead code";
 const char *GIT_BRANCH =	"lvgl8-observer";
-const char *GIT_COMMIT_DATE =	"Wed, 7 Feb 2024 23:25:47 +0100";
+const char *GIT_COMMIT_DATE =	"Thu, 8 Feb 2024 00:01:59 +0100";
 
 #endif
