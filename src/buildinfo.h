082c1e1716ad08629d0be3faf547df26ea449356
Michael Haberler <git@mah.priv.at>
2024-02-11 19:10:37 +0100

    add color-swapped Squarline export for T4S3

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "52ba440-dirty";
+const char *GIT_REV = "6f9efd2-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"fix BLE sensor timeouts";
+const char *GIT_SUBJECT =	"make LilyGo T4-S3 work with Arduino3.0";
 const char *GIT_BRANCH =	"lvgl8-observer";
-const char *GIT_COMMIT_DATE =	"Sun, 11 Feb 2024 18:30:22 +0100";
+const char *GIT_COMMIT_DATE =	"Sun, 11 Feb 2024 18:46:06 +0100";
 
 #endif
---
+++ test-ui-swapped-colors/project/SquareLine_Project_events.py
@@ -0,0 +1,28 @@
+
+def showMemory(event_struct):
+    return
+
+
+def longPressed(event_struct):
+    return
+
+
+def ruuviEnvelope(event_struct):
+    return
+
+
+def ruuviOAT(event_struct):
+    return
+
+
+def ruuviCancel(event_struct):
+    return
+
+
+def setUnit(event_struct):
+    return
+
+
+def wipeConfig(event_struct):
+    return
+
