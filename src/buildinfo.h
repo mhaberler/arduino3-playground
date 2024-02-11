4c11da800aa09e57695be42de3c7cf49c9e57269
Michael Haberler <git@mah.priv.at>
2024-02-11 07:08:37 +0100

    color-unswapped Squareline export

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "f47ff9b-dirty";
+const char *GIT_REV = "b36eed4-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"touch works";
-const char *GIT_BRANCH =	"lilygo_t4s3";
-const char *GIT_COMMIT_DATE =	"Sun, 11 Feb 2024 06:06:17 +0100";
+const char *GIT_SUBJECT =	"NFC needs lvgl locking around nfc ops";
+const char *GIT_BRANCH =	"lvgl8-observer";
+const char *GIT_COMMIT_DATE =	"Sun, 11 Feb 2024 06:34:04 +0100";
 
 #endif
