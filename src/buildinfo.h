eb63946ebcf7d31407942c0e76b51c1058550063
Michael Haberler <git@mah.priv.at>
2024-02-11 23:18:01 +0100

    remark on lovyangfx+cores3+red port

--- src/buildinfo.h
+++ src/buildinfo.h
@@ -3,10 +3,10 @@
 
 #define GIT_REPO_PRESENT 1
 
-const char *GIT_REV = "2b374db-dirty";
+const char *GIT_REV = "32eac1d-dirty";
 const char *GIT_AUTHOR =	"Michael Haberler";
-const char *GIT_SUBJECT =	"ini changes for color-swapped export";
+const char *GIT_SUBJECT =	"fix Mopeka battery reporting";
 const char *GIT_BRANCH =	"lvgl8-observer";
-const char *GIT_COMMIT_DATE =	"Sun, 11 Feb 2024 19:10:51 +0100";
+const char *GIT_COMMIT_DATE =	"Sun, 11 Feb 2024 19:20:32 +0100";
 
 #endif
